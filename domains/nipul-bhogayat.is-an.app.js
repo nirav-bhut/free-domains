c0b3a5448e1976f79af920de556a56e65af9d998
ppbackend <purva.patel@acquaintsoft.com>
2023-04-21 11:53:16 +0530

    Add .is-an.app.js file

---
+++ domains/nipul-bhogayat.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+    description: 'I want to publish my profile',
+    domain: 'is-an.app',
+    subdomain: 'nipul-bhogayat',
+    owner: {
+      repo: 'https://github.com/nipul-bhogayat/nipul-bhogayat.github.io',
+      email: 'nipul.bhogayata@truestaz.com ',
+    },
+    record: {
+      CNAME: 'nipul-bhogayat.github.io',
+    },
+    proxy: false,
+  })
